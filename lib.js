4cbea4713f884ef88587754c83b91ffd9e258892
Etienne Rossignon <etienne.rossignon@gadz.org>
2014-05-24 17:57:27 +0200

    test with utf file starting with BOM

--- lib.js
+++ lib.js
@@ -1,45 +1,127 @@
+/**
+ * License MIT
+ */
 var util = require('util');
-var xml = require("node-xml");
 var EventEmitter = require("events").EventEmitter;
-var Parser = function(encoding)
-{
 
-  function _toMap(arr) {
-    var m = {}
-    arr.forEach(function(pair) {
-        m[pair[0]]=pair[1];
+/**
+ * @class Parser
+ * @param encoding
+ * @constructor
+ */
+var Parser = function (encoding) {
+
+    this._text = [];
+
+    function _toMap(arr) {
+        var m = {}
+        arr.forEach(function (pair) {
+            m[pair[0]] = pair[1];
+        });
+        return m;
+    }
+
+    var self = this;
+    var xml = require("node-xml");
+    self._xml = new xml.SaxParser(function (cb) {
+
+        cb.onStartDocument(function () {
+
+        });
+        cb.onEndDocument(function () {
+            self.emit("close");
+        });
+        cb.onStartElementNS(function (elem, attrs, prefix, uri, namespaces) {
+            self.emit("startElement", elem, _toMap(attrs));
+        });
+        cb.onEndElementNS(function (elem, prefix, uri) {
+            self.emit("endElement", elem);
+        });
+        cb.onCharacters(function (chars) {
+            chars.split("\n").forEach(function (chunk) {
+                self.emit("text", chunk);
+            });
+        });
     });
-    return m;
-  }
-  var self = this;
-  self._xml = new xml.SaxParser(function(cb){
- 
-          cb.onStartDocument(function() {
-          });
-          cb.onEndDocument(function() {
-              self.emit("close");
-          });
-          cb.onStartElementNS(function(elem,attrs,prefix,uri,namespaces) {
-            self.emit("startElement",elem,_toMap(attrs)); 
-          });
-          cb.onEndElementNS(function(elem,prefix,uri) {
-            self.emit("endElement",elem);
-          });
-          cb.onCharacters(function(chars){
-            chars.split("\n").forEach(function(chunk){ self.emit("text",chunk);});
-          });
-  });
 
-}
-util.inherits(Parser,EventEmitter);
-Parser.prototype.write = function(str) {
-   this._xml.parseString(str);
+};
+util.inherits(Parser, EventEmitter);
+
+Parser.prototype.write = function (str) {
+    this._text.push(str.toString());
 };
 
-Parser.prototype.end = function() {
+Parser.prototype.end = function (data) {
+    if (data) {
+        this.write(data);
+    }
+    var aa = this._text.join("");
+    this._xml.parseString(aa);
 };
-
 
 exports.Parser = Parser;
+/*
+var fs = require("fs");
+var expat1 = __dirname + "/../node-expat";
+var expat2 = __dirname + "/node_modules/node-expat";
+if ((fs.existsSync(expat1) || fs.existsSync(expat2))) {
+    console.log(" using fast node-expat");
+    exports.Parser = require("node-expat").Parser;
+} else {
+    exports.Parser = Parser;
+}
 
+// var xml_lite = require("./node_modules/node-xml-lite/index.js");
+var xml_lite = require("node-xml-lite");
 
+var Parser2 = function () {
+    // var xml_lite = require("./aa");
+    this._xml = new xml_lite.XMLParser();
+    this._stack =[];
+
+};
+util.inherits(Parser2, EventEmitter);
+
+Parser2.prototype.write = function (str) {
+
+    var buf = str; //new Buffer(str);
+    var self = this;
+
+    this._xml.parseBuffer(buf, buf.length, function (state, p1, p2) {
+
+        console.log("state",state);
+        if (self._wait_attrib === true && state != xml_lite.xtAttribute) {
+            self.emit("startElement",self._elementName,self._attrib);
+            self._wait_attrib=false;
+        }
+        switch(state) {
+            case xml_lite.xtOpen:
+                self._attrib = {};
+                self._wait_attrib = true;
+                self._elementName = p1;
+                self._stack.push(p1);
+                break;
+            case xml_lite.xtClose:
+                var name = self._stack.pop();
+                self.emit("endElement",name);
+                if (self._stack.length === 0 ) {
+                    console.log("done");
+                    self.emit("close");
+                }
+                break;
+            case xml_lite.xtAttribute:
+                self._attrib[p1] = p2;
+                break;
+            case xml_lite.xtText:
+                self.emit("text", p1);
+        }
+        return true;
+    });
+};
+
+Parser2.prototype.end = function (str) {
+    var self = this;
+    if (str) { self.write(str);}
+};
+exports.Parser = Parser;
+*/
--- test/test_node-xml.js
+++ test/test_node-xml.js
@@ -1,38 +1,38 @@
 /*global require,describe, it*/
 var expat = require('node-expat');
-var ersatzt = require("../lib");
+var ersatz = require("../lib");
 var should = require("should");
+
+
+var fs = require("fs");
+
 
 describe("compare node-expat  and erstatz-node-expat", function () {
 
 
     function perform_test(namespace) {
-        var parser = new namespace.Parser('UTF-8');
 
+        var parser = new namespace.Parser('UTF-8');
 
         var infos = [];
 
         parser.on('startElement', function (name, attrs) {
-            console.log("startElement", name, attrs)
-            infos.push(['startElement',name,attrs]);
+            infos.push(['startElement', name, attrs]);
         });
 
         parser.on('endElement', function (name) {
-            console.log("endElement", name)
-            infos.push(['endElement',name]);
+            infos.push(['endElement', name]);
         });
 
         parser.on('text', function (text) {
-            console.log(" text : <" + text + ">");
-            text = text.split("\n").join("");
-            if (text.length >0) {
-                infos.push(['text',text.trim()]);
+            text = text.split("\n").join("").trim();
+            if (text.length > 0) {
+                infos.push(['text', text]);
             }
         });
 
         parser.on('error', function (error) {
-            console.error(error);
-            infos.push(['error',error]);
+            infos.push(['error', error]);
         });
 
         var xml_string = require("fs").readFileSync(__dirname + "/demo.xml");
@@ -42,9 +42,56 @@ describe("compare node-expat  and erstatz-node-expat", function () {
     }
 
     it("node-expat", function () {
-       var infos1 =  perform_test(expat);
-       var infos2 = perform_test(ersatzt);
-       infos1.should.eql(infos2);
+        var infos1 = perform_test(expat);
+        var infos2 = perform_test(ersatz);
+        infos1.should.eql(infos2);
+
+    });
+
+    function performTestAsync(namespace, xmlfile, callback) {
+
+        var map = {};
+
+        function record(name) {
+            if (map[name]) {
+                map[name] += 1;
+            } else {
+                map[name] = 1;
+            }
+        }
+
+        var parser = new namespace.Parser();
+        parser.on('startElement', function (name, attrs) {
+            record(name);
+        });
+        parser.on('endElement', function (name) {
+        });
+        parser.on('text', function (text) {
+        });
+        parser.on("close", function () {
+            callback(map);
+        });
+        var fs = require('fs');
+        var bomstrip = require('bomstrip');
+        fs.createReadStream(xmlfile, "utf8").pipe(new bomstrip()).pipe(parser);
+    }
+
+
+    it("should handle a small xml file", function (done) {
+        var xmlfile = __dirname + "/demo.xml";
+        performTestAsync(ersatz, xmlfile, function (map) {
+            Object.keys(map).length.should.be.greaterThan(2);
+            done();
+        });
+
+    });
+    it("should handle a utf8 xml file with a BOM", function (done) {
+        // http://stackoverflow.com/questions/6302544/default-encoding-for-xml-is-utf-8-or-utf-16
+        var xmlfile = __dirname + "/utf8-with_bom_example.xml";
+        performTestAsync(ersatz, xmlfile, function (map) {
+            Object.keys(map).length.should.be.greaterThan(2);
+            done();
+        });
 
     });
 });
