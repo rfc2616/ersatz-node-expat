d6c165dd438048505edf9d7932cfaa0ac7392bb1
Etienne Rossignon <etienne.rossignon@gadz.org>
2015-11-09 23:11:47 +0100

    fix issue with elements containing escaped characters

--- test/test_node-xml.js
+++ test/test_node-xml.js
@@ -1,5 +1,11 @@
 /*global require,describe, it*/
-var expat = require('node-expat');
+var expat;
+try {
+    expat = require('node-expat');
+}
+catch(err) {
+
+}
 var ersatz = require("../lib");
 var should = require("should");
 
@@ -10,7 +16,7 @@ var fs = require("fs");
 describe("compare node-expat  and erstatz-node-expat", function () {
 
 
-    function perform_test(namespace) {
+    function perform_test(namespace, xml_string) {
 
         var parser = new namespace.Parser('UTF-8');
 
@@ -35,17 +41,28 @@ describe("compare node-expat  and erstatz-node-expat", function () {
             infos.push(['error', error]);
         });
 
-        var xml_string = require("fs").readFileSync(__dirname + "/demo.xml");
         parser.write(xml_string);
         parser.end();
 
         return infos;
     }
 
-    it("node-expat", function () {
-        var infos1 = perform_test(expat);
-        var infos2 = perform_test(ersatz);
-        infos1.should.eql(infos2);
+    if (expat) {
+        it("node-expat", function () {
+            var xml_string = require("fs").readFileSync(__dirname + "/demo.xml");
+            var infos1 = perform_test(expat, xml_string);
+            var infos2 = perform_test(ersatz, xml_string);
+            infos1.should.eql(infos2);
+
+        });
+    }
+
+    it("should handle a  xml file with encoded string in text", function (done) {
+        var xmlfile = __dirname + "/issue.xml";
+        var xml_string = require("fs").readFileSync(xmlfile);
+        var infos1 = perform_test(ersatz, xml_string);
+        infos1[1][1].should.eql("<Hello World>");
+        done();
 
     });
 
@@ -86,6 +103,8 @@ describe("compare node-expat  and erstatz-node-expat", function () {
         });
 
     });
+
+
     it("should handle a utf8 xml file with a BOM", function (done) {
         // http://stackoverflow.com/questions/6302544/default-encoding-for-xml-is-utf-8-or-utf-16
         var xmlfile = __dirname + "/utf8-with_bom_example.xml";
